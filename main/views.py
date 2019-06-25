20465894a820e0875fb5178107ef3890a274922d
Janish <JSjanish.1999@gmail.com>
2019-06-26 00:06:25 +0530

    First Commit

---
+++ main/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+
+from django.views.generic.edit import FormView
+from main import forms
+
+
+class ContactUsView(FormView):
+    template_name = "contact_us.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
