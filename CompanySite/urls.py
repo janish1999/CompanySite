50bc67afd31d3969b5da81d97b46e7dcdffd05c0
Janish <JSjanish.1999@gmail.com>
2019-06-27 03:15:21 +0530

    Tempaltes Commit

--- CompanySite/urls.py
+++ CompanySite/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.detail import DetailView
+from main import models
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"),name="home"),
     path('about-us/', TemplateView.as_view(template_name="about_us.html"),name="about_us"),
     path("contact-us/", views.ContactUsView.as_view(template_name="contact_us.html"), name="contact_us"),
-]
+    path("products/<slug:tag>/",views.ProductListView.as_view(),name="product"),
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product),name="product",),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -3,7 +3,33 @@ from django.db import models
 # Create your models here.
 
 
+class ActiveManager(models.Manager):
+    def active(self):
+        return self.filter(active=True)
+
+
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+
+class ProductTag(models.Model):
+    objects = ProductTagManager()
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return self.slug,
+
+
 class Product(models.Model):
+    objects = ActiveManager()
+    tags = models.ManyToManyField(ProductTag, blank=True)
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -11,6 +37,9 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(models.Model):
@@ -22,9 +51,4 @@ class ProductImage(models.Model):
         upload_to="product-thumbnails", null=True)
 
 
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
+
