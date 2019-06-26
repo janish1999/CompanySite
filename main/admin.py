b20de63a60837c906c3c5320ff0435820b7e8a79
Janish <JSjanish.1999@gmail.com>
2019-06-27 03:32:26 +0530

    Added User

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from . import models
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -34,6 +35,49 @@ class ProductImageAdmin(admin.ModelAdmin):
 
     def product_name(self, obj):
         return obj.product.name
+
+
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {"fields": ("last_login", "date_joined")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
 
 
 admin.site.register(models.Product, ProductAdmin)
